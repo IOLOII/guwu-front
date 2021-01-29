506dd8223f5257f4f91595f4d1b26e91704e6c56
IOLOII <yuey@coinioc.cc>
2021-01-29 21:40:35 +0800

    api代理

--- vue.config.js
+++ vue.config.js
@@ -36,7 +36,20 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    before: require('./mock/mock-server.js'),
+    proxy: {
+      '/dev-api/guwu': {
+        target: 'http://localhost:2414/', // 设置你调用的接口域名 别忘了加http
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dev-api/guwu': '/'// 这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://m.kugou.com/rank/info/?rankid=23784&page=1&json=true'，直接写‘/api/rank/info/?rankid=23784&page=1&json=true’即可
+        }
+      }
+      //   '/dev-api/guwu': {
+      //     target: 'http://localhost:2414',
+      //     pathRewrite: { '^/dev-api/guwu': '/' }
+      //   }
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
