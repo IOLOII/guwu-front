d27a4ef0bbbc3df8b26d913db930f646a122631a
IOLOII <yuey@coinioc.cc>
2021-04-16 14:31:38 +0800

    Update:调整首页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -152,9 +152,11 @@ export default {
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg: #283443;
+$bg: #b4b4b48a;
 $light_gray: #fff;
 $cursor: #fff;
+$height: 52px;
+$radius: 12px;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
@@ -166,17 +168,17 @@ $cursor: #fff;
 .login-container {
   .el-input {
     display: inline-block;
-    height: 47px;
+    height: 52px;
     width: 85%;
 
     input {
       background: transparent;
       border: 0px;
       -webkit-appearance: none;
-      border-radius: 0px;
+      border-radius: $radius;
       padding: 12px 5px 12px 15px;
       color: $light_gray;
-      height: 47px;
+      height: $height;
       caret-color: $cursor;
 
       &:-webkit-autofill {
@@ -187,17 +189,23 @@ $cursor: #fff;
   }
 
   .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
+    background: rgb(240 240 240 / 15%);
+    border: 2px solid rgb(255 255 255);
+    border-radius: $radius;
+  }
+  button {
+    height: $height;
+    border-radius: $radius;
+    span {
+      font-size: 1.2rem;
+    }
   }
 }
 </style>
 
 <style lang="scss" scoped>
 $bg: #2d3a4b;
-$dark_gray: #889aa4;
+$dark_gray: #ffffff;
 $light_gray: #eee;
 
 .login-container {
@@ -205,6 +213,10 @@ $light_gray: #eee;
   width: 100vw;
   // background-color: $bg;
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   video {
     position: absolute;
     /* Vertical and Horizontal center*/
@@ -221,7 +233,7 @@ $light_gray: #eee;
     position: relative;
     width: 520px;
     max-width: 100%;
-    padding: 160px 35px 0;
+    padding: 0 35px 0;
     margin: 0 auto;
     overflow: hidden;
   }
